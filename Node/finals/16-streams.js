e855b3b0639ff5e65e341c3146c866ed84d951ae
Srikanth <sri1@publicisgroupe.net>
2022-01-07 17:47:33 +0530

    streams files updated

---
+++ Node/finals/16-streams.js
@@ -0,0 +1,17 @@
+const { createReadStream } = require('fs');
+
+const stream = createReadStream('./content/big.txt', {highWaterMark: 90000, encoding: 'utf-8'});
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+
+stream.on('data', (result)=>{
+    console.log(result);
+})
+
+stream.on('error', (err)=>{
+    console.log(err);
+})
