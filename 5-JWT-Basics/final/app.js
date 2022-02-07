8d90cae696b1c3f14780732e6e1dce77730da97b
Srikanth <sri1@publicisgroupe.net>
2022-02-07 17:43:15 +0530

    JWT project completed

--- 5-JWT-Basics/final/app.js
+++ 5-JWT-Basics/final/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const mainRouter = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -12,6 +13,7 @@ app.use(express.static('./public'));
 app.use(express.json());
 
 
+app.use('/api/v1', mainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ 5-JWT-Basics/final/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('./../errors/custom-error');
+
+const authenticationMiddleware = async(req, res, next) => {
+    
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer')){
+        throw new CustomAPIError('No Token Provided', 401)
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const {id, username} = decoded;
+        req.user = {id, username}
+        next();
+    } catch (error) {
+        throw new CustomAPIError('Not Authorized to access this route', 401)
+    }
+
+}
+
+module.exports = authenticationMiddleware;
