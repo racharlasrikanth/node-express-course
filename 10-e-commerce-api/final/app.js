d968f7570bfc19ea56f7f984a613a5369a9f1d35
Srikanth <sri1@publicisgroupe.net>
2022-03-20 12:43:33 +0530

    connected to db, basic get route added

--- 10-e-commerce-api/final/app.js
+++ 10-e-commerce-api/final/app.js
@@ -1,1 +1,43 @@
-console.log("hello srikanth");
+require('dotenv').config();
+require('express-async-errors');
+
+// express
+const express = require('express');
+const app = express();
+
+// rest of the packages
+const morgan = require('morgan');
+
+// database
+const connectDB = require('./db/connect');
+
+// middleware
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+
+app.use(morgan('tiny'));
+// access json data from req.body
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('e-commerce api')
+})
+
+// middlewares
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        console.log('CONNECTED');
+        app.listen(PORT, () => {
+            console.log(`Server is listening on PORT ${PORT}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
--- 10-e-commerce-api/final/db/connect.js
+++ 10-e-commerce-api/final/db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
+    console.log("Connecting to DB...");
     return mongoose.connect(url);
 }
 
