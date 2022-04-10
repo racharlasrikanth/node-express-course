8cda7596c114452a0e648137726a9a73efbf7c5c
Srikanth <sri1@publicisgroupe.net>
2022-04-10 17:30:54 +0530

    mongoose virtual added for reviews vs products

--- 10-e-commerce-api/final/controllers/productController.js
+++ 10-e-commerce-api/final/controllers/productController.js
@@ -17,7 +17,7 @@ const getAllProducts = async (req, res) => {
 const getSingleProduct = async (req, res) => {
     const {id:productId} = req.params;
 
-    const product = await Product.findOne({_id: productId});
+    const product = await Product.findOne({_id: productId}).populate('reviews');
 
     if (!product) {
         throw new CustomError.NotFoundError(`No product with id: ${productId}`);
--- 10-e-commerce-api/final/models/Product.js
+++ 10-e-commerce-api/final/models/Product.js
@@ -60,6 +60,13 @@ const ProductSchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     }
-}, {timestamps: true});
+}, {timestamps: true, toJSON:{virtuals: true}, toObject:{virtuals: true}});
+
+ProductSchema.virtual('reviews', {
+    ref: 'Review',
+    localField: '_id',
+    foreignField: 'product',
+    justOne: false,
+})
 
 module.exports = mongoose.model('Product', ProductSchema);
