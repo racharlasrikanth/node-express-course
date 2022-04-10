3b81c27d5890caf558662e965b9285e3360acc94
Srikanth <sri1@publicisgroupe.net>
2022-04-10 12:41:51 +0530

    product model created

---
+++ 10-e-commerce-api/final/models/Product.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide product name'],
+        trim: true,
+        maxlength: [100, 'Name can not be more thatn 100 characters']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please provide product price'],
+        default: 0,
+    },
+    description: {
+        type: String,
+        required: [true, 'Please provide product description'],
+        maxlength: [1000, 'Description cannot be more than 1000 characters']
+    },
+    image: {
+        type: String,
+        default: '/uploads/example.jpeg'
+    },
+    category: {
+        type: String,
+        required: [true, 'Please provide product category'],
+        enum: ['office', 'kitchen', 'bedroom']
+    },
+    company: {
+        type: String,
+        required: [true, 'Please provide company'],
+        enum: {
+            values: ['ikea', 'liddy', 'marcos'],
+            message: '{VALUE} is not supported',
+        }
+    },
+    colors: {
+        type: [String],
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+    freeShipping: {
+        type: Boolean,
+        default: false,
+    },
+    inventory: {
+        type: Number,
+        required: true,
+        default: 15,
+    },
+    averageRating: {
+        type: Number,
+        default: 0,
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Product', ProductSchema);
