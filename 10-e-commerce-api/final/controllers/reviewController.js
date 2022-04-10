446f57c83091db9a4fedfae85dbf423a1fda7b36
Srikanth <sri1@publicisgroupe.net>
2022-04-10 17:16:41 +0530

    populate method for getting more info about a collection

--- 10-e-commerce-api/final/controllers/reviewController.js
+++ 10-e-commerce-api/final/controllers/reviewController.js
@@ -30,7 +30,7 @@ const createReview = async (req, res) => {
 }
 
 const getAllReviews = async (req, res) => {
-    const reviews = await Review.find({});
+    const reviews = await Review.find({}).populate({path:'product', select:'name company price'}).populate({path:'user', select:'name email'});
 
     res.status(StatusCodes.OK).json({count:reviews.length, reviews})
 }
